1f4560c72f52a214d993db2893a926feeed42ea8
Sahil <hanss9545@gmail.com>
2023-10-19 08:12:25 +0530

    Latest Trends component design completed for desktop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Home from "./pages/Home";
+import Products from "./pages/searchedProducts";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Products />
     </div>
   );
 }
