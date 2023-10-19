11731e7eba5d5c0a141cf51547aa5d76afa8b7f7
Sahil <hanss9545@gmail.com>
2023-10-19 08:56:47 +0530

    Product card design completed for desktop

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,6 +1,35 @@
 import React from "react";
-import product1 from "../../assets/images/product1.png";
+import goldStar from "../../assets/images/goldStar.svg";
 
-const ProductCard = () => {};
+const ProductCard = ({
+  imgUrl,
+  productName,
+  oldPrice,
+  CurrentPrice,
+  stars,
+  starNum,
+}) => {
+  return (
+    <div className="productCard rounded overflow-hidden">
+      <img src={imgUrl} alt="" />
+      <h3 className="text-xl mt-6">{productName}</h3>
+      <p className="mt-3">
+        <span className="text-xl text-[#0006] line-through">
+          Rs. {oldPrice}
+        </span>
+        &nbsp;
+        <span className="text-xl text-[#6D84FF]">Rs.{CurrentPrice}</span>
+      </p>
+      <div className="mt-3 flex gap-1 items-center">
+        <div className="flex gap-[2px]">
+          {[1, 2, 3, 4, 5].map(() => {
+            return <img src={goldStar} alt="" />;
+          })}
+        </div>
+        <span className="relative bottom-[2px]">({starNum})</span>
+      </div>
+    </div>
+  );
+};
 
 export default ProductCard;
--- src/pages/searchedProducts/index.js
+++ src/pages/searchedProducts/index.js
@@ -3,6 +3,14 @@ import Logo from "../../components/logo";
 import SearchBar from "../../components/searchBar/SearchBar";
 import "./style.scss";
 import ProductCard from "../../components/productCard/ProductCard";
+import product1 from "../../assets/images/product1.png";
+import product2 from "../../assets/images/product2.png";
+import product3 from "../../assets/images/product3.png";
+import product4 from "../../assets/images/product4.png";
+import product5 from "../../assets/images/product5.png";
+import product6 from "../../assets/images/product6.png";
+import product7 from "../../assets/images/product7.png";
+import product8 from "../../assets/images/product8.png";
 
 const Products = () => {
   return (
@@ -18,14 +26,70 @@ const Products = () => {
           <div className="w-[350px]"></div>
           <div className=" flex-1">
             <div className="productsGrid">
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
+              <ProductCard
+                imgUrl={product1}
+                productName="Round neck cotton Tee"
+                oldPrice="599"
+                CurrentPrice="549"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product2}
+                productName="High waist trousers"
+                oldPrice="999"
+                CurrentPrice="749"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product3}
+                productName="High neck pullover"
+                oldPrice="899"
+                CurrentPrice="849"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product4}
+                productName="satin jumpsuit"
+                oldPrice="650"
+                CurrentPrice="549"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product5}
+                productName="High waist trousers"
+                oldPrice="999"
+                CurrentPrice="749"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product6}
+                productName="High waist trousers"
+                oldPrice="999"
+                CurrentPrice="749"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product7}
+                productName="High waist trousers"
+                oldPrice="999"
+                CurrentPrice="749"
+                stars={5}
+                starNum={210}
+              />
+              <ProductCard
+                imgUrl={product8}
+                productName="High waist trousers"
+                oldPrice="999"
+                CurrentPrice="749"
+                stars={5}
+                starNum={210}
+              />
             </div>
           </div>
         </div>
